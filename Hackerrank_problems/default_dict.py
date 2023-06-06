8f03b160771327e4c71f106d3ec92280c5fc078e
Japus <japusjapus1134@gmail.com>
2023-06-06 20:08:47 +0530

    Updated default_dict problem 6/6/23

--- Hackerrank_problems/default_dict.py
+++ Hackerrank_problems/default_dict.py
@@ -1,36 +1,14 @@
 from collections import defaultdict
 
-m, n = list(map(int, input().split()))
-
-m_list = defaultdict(list)
-for i in range(1, m+1):
-    m_list[i].append((input()))
-# print(m_list)
-
-n_list = []
-for j in range(1,n+1):
-    n_list.append(input())
-# print(n_list)
-
 
 d = defaultdict(list)
-for i in n_list:
-    for j in range(1, len(m_list)+1):
-        if i in m_list[j]:
-            # print(i)
-            # print(m_list[j])
-            # print(j)
-            d[i].append(j)
+m, n = list(map(int, input().split()))
 
-for i in range(1, len(d.items())+1):
-    print(i)
-    print(d)
+# getting the string and index position on the dict list
+for i in range(m):
+    d[input()].append(str(i+1))
 
-
-
-
-
-
-
-
+# getting the string position of group B in group A:
+for j in range(n):
+    print(" ".join(d[input()]) or -1)
 
